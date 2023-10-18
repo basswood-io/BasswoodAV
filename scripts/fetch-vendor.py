4416cba6f5e6ec03936c8b6c486ee32121824aa7
WyattBlue <wyattblue@auto-editor.com>
2023-10-18 17:48:11 -0400

    Simply fetch-vendor script

--- scripts/fetch-vendor.py
+++ scripts/fetch-vendor.py
@@ -1,64 +1,53 @@
+from struct import calcsize
 import argparse
-import logging
-import json
 import os
 import platform
 import shutil
-import struct
 import subprocess
 
 
 def get_platform():
     system = platform.system()
     machine = platform.machine()
+
     if system == "Linux":
         return f"manylinux_{machine}"
-    elif system == "Darwin":
+
+    if system == "Darwin":
         # cibuildwheel sets ARCHFLAGS:
         # https://github.com/pypa/cibuildwheel/blob/5255155bc57eb6224354356df648dc42e31a0028/cibuildwheel/macos.py#L207-L220
         if "ARCHFLAGS" in os.environ:
             machine = os.environ["ARCHFLAGS"].split()[1]
         return f"macosx_{machine}"
-    elif system == "Windows":
-        if struct.calcsize("P") * 8 == 64:
-            return "win_amd64"
-        else:
-            return "win32"
-    else:
-        raise Exception(f"Unsupported system {system}")
+
+    if system == "Windows":
+        return "win_amd64" if calcsize("P") * 8 == 64 else "win32"
+
+    raise Exception(f"Unsupported system {system}")
 
 
 parser = argparse.ArgumentParser(description="Fetch and extract tarballs")
 parser.add_argument("destination_dir")
 parser.add_argument("--cache-dir", default="tarballs")
-parser.add_argument("--config-file", default=os.path.splitext(__file__)[0] + ".json")
 args = parser.parse_args()
-logging.basicConfig(level=logging.INFO)
 
-# read config file
-with open(args.config_file) as fp:
-    config = json.load(fp)
-
-# create fresh destination directory
-logging.info("Creating directory %s" % args.destination_dir)
+print(f"Creating directory {args.destination_dir}")
 if os.path.exists(args.destination_dir):
     shutil.rmtree(args.destination_dir)
 os.makedirs(args.destination_dir)
 
-for url_template in config["urls"]:
-    tarball_url = url_template.replace("{platform}", get_platform())
+tarball_url = f"https://github.com/PyAV-Org/pyav-ffmpeg/releases/download/5.1.2-1/ffmpeg-{get_platform()}.tar.gz"
 
-    # download tarball
-    tarball_name = tarball_url.split("/")[-1]
-    tarball_file = os.path.join(args.cache_dir, tarball_name)
-    if not os.path.exists(tarball_file):
-        logging.info("Downloading %s" % tarball_url)
-        if not os.path.exists(args.cache_dir):
-            os.mkdir(args.cache_dir)
-        subprocess.check_call(
-            ["curl", "--location", "--output", tarball_file, "--silent", tarball_url]
-        )
+tarball_name = tarball_url.split("/")[-1]
+tarball_file = os.path.join(args.cache_dir, tarball_name)
 
-    # extract tarball
-    logging.info("Extracting %s" % tarball_name)
-    subprocess.check_call(["tar", "-C", args.destination_dir, "-xf", tarball_file])
+if not os.path.exists(tarball_file):
+    print(f"Downloading {tarball_url}")
+    if not os.path.exists(args.cache_dir):
+        os.mkdir(args.cache_dir)
+    subprocess.run(
+        ["curl", "--location", "--output", tarball_file, "--silent", tarball_url]
+    )
+
+print(f"Extracting {tarball_name}")
+subprocess.run(["tar", "-C", args.destination_dir, "-xf", tarball_file])
