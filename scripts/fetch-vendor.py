77136a69216ad191a07be21ea0d4100d49b53009
WyattBlue <wyattblue@auto-editor.com>
2023-10-21 05:01:34 -0400

    Try using ffmpeg 6.0 wheels

--- scripts/fetch-vendor.py
+++ scripts/fetch-vendor.py
@@ -6,22 +6,26 @@ import shutil
 import subprocess
 
 
-def get_platform():
+def get_url():
     system = platform.system()
     machine = platform.machine()
 
     if system == "Linux":
-        return f"manylinux_{machine}"
+        plat = f"manylinux_{machine}"
 
     if system == "Darwin":
         # cibuildwheel sets ARCHFLAGS:
         # https://github.com/pypa/cibuildwheel/blob/5255155bc57eb6224354356df648dc42e31a0028/cibuildwheel/macos.py#L207-L220
         if "ARCHFLAGS" in os.environ:
             machine = os.environ["ARCHFLAGS"].split()[1]
-        return f"macosx_{machine}"
+        plat = f"macosx_{machine}"
+
+    if system == "Linux" or system == "Darwin":
+        return f"https://github.com/WyattBlue/pyav-ffmpeg/releases/download/6.0-1/ffmpeg-{plat}.tar.gz"
 
     if system == "Windows":
-        return "win_amd64" if calcsize("P") * 8 == 64 else "win32"
+        plat = "win_amd64" if calcsize("P") * 8 == 64 else "win32"
+        return f"https://github.com/PyAV-Org/pyav-ffmpeg/releases/download/5.1.2-1/ffmpeg-{plat}.tar.gz"
 
     raise Exception(f"Unsupported system {system}")
 
@@ -36,8 +40,7 @@ if os.path.exists(args.destination_dir):
     shutil.rmtree(args.destination_dir)
 os.makedirs(args.destination_dir)
 
-tarball_url = f"https://github.com/PyAV-Org/pyav-ffmpeg/releases/download/5.1.2-1/ffmpeg-{get_platform()}.tar.gz"
-
+tarball_url = get_url()
 tarball_name = tarball_url.split("/")[-1]
 tarball_file = os.path.join(args.cache_dir, tarball_name)
 
