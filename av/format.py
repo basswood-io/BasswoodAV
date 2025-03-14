54c698a26b3d7e736a841540cd771b3659e8d647
WyattBlue <wyattblue@auto-editor.com>
2025-03-14 13:55:17 -0400

    Make format.pyx pure

--- av/format.py
+++ av/format.py
@@ -1,16 +1,23 @@
-cimport libav as lib
-
-from av.descriptor cimport wrap_avclass
-
 from enum import Flag
 
+import cython
+from cython.cimports import libav as lib
+from cython.cimports.av.descriptor import wrap_avclass
 
-cdef object _cinit_bypass_sentinel = object()
+_cinit_bypass_sentinel = cython.declare(object, object())
 
-cdef ContainerFormat build_container_format(lib.AVInputFormat* iptr, lib.AVOutputFormat* optr):
+
+@cython.cfunc
+def build_container_format(
+    iptr: cython.pointer[lib.AVInputFormat], optr: cython.pointer[lib.AVOutputFormat]
+) -> ContainerFormat:
     if not iptr and not optr:
         raise ValueError("needs input format or output format")
-    cdef ContainerFormat format = ContainerFormat.__new__(ContainerFormat, _cinit_bypass_sentinel)
+
+    format = cython.declare(
+        ContainerFormat,
+        ContainerFormat.__new__(ContainerFormat, _cinit_bypass_sentinel),
+    )
     format.iptr = iptr
     format.optr = optr
     format.name = optr.name if optr else iptr.name
@@ -22,14 +29,18 @@ class Flags(Flag):
     need_number: "Needs '%d' in filename." = lib.AVFMT_NEEDNUMBER
     show_ids: "Show format stream IDs numbers." = lib.AVFMT_SHOW_IDS
     global_header: "Format wants global header." = lib.AVFMT_GLOBALHEADER
-    no_timestamps: "Format does not need / have any timestamps." = lib.AVFMT_NOTIMESTAMPS
+    no_timestamps: "Format does not need / have any timestamps." = (
+        lib.AVFMT_NOTIMESTAMPS
+    )
     generic_index: "Use generic index building code." = lib.AVFMT_GENERIC_INDEX
     ts_discont: "Format allows timestamp discontinuities" = lib.AVFMT_TS_DISCONT
     variable_fps: "Format allows variable fps." = lib.AVFMT_VARIABLE_FPS
     no_dimensions: "Format does not need width/height" = lib.AVFMT_NODIMENSIONS
     no_streams: "Format does not require any streams" = lib.AVFMT_NOSTREAMS
     no_bin_search: "Format does not allow to fall back on binary search via read_timestamp" = lib.AVFMT_NOBINSEARCH
-    no_gen_search: "Format does not allow to fall back on generic search" = lib.AVFMT_NOGENSEARCH
+    no_gen_search: "Format does not allow to fall back on generic search" = (
+        lib.AVFMT_NOGENSEARCH
+    )
     no_byte_seek: "Format does not allow seeking by bytes" = lib.AVFMT_NO_BYTE_SEEK
     allow_flush: "Format allows flushing. If not set, the muxer will not receive a NULL packet in the write_packet function." = lib.AVFMT_ALLOW_FLUSH
     ts_nonstrict: "Format does not require strictly increasing timestamps, but they must still be monotonic." = lib.AVFMT_TS_NONSTRICT
@@ -38,7 +49,9 @@ class Flags(Flag):
     # so they start from 0. The user or muxer can override this through AVFormatContext.avoid_negative_ts
     seek_to_pts: "Seeking is based on PTS" = lib.AVFMT_SEEK_TO_PTS
 
-cdef class ContainerFormat:
+
+@cython.cclass
+class ContainerFormat:
     """Descriptor of a container format.
 
     :param str name: The name of the format.
@@ -60,7 +73,7 @@ cdef class ContainerFormat:
             self.iptr = lib.av_find_input_format(name)
 
         if mode is None or mode == "w":
-            self.optr = lib.av_guess_format(name, NULL, NULL)
+            self.optr = lib.av_guess_format(name, cython.NULL, cython.NULL)
 
         if not self.iptr and not self.optr:
             raise ValueError(f"no container format {name!r}")
@@ -82,30 +95,30 @@ cdef class ContainerFormat:
     @property
     def input(self):
         """An input-only view of this format."""
-        if self.iptr == NULL:
+        if self.iptr == cython.NULL:
             return None
-        elif self.optr == NULL:
+        elif self.optr == cython.NULL:
             return self
         else:
-            return build_container_format(self.iptr, NULL)
+            return build_container_format(self.iptr, cython.NULL)
 
     @property
     def output(self):
         """An output-only view of this format."""
-        if self.optr == NULL:
+        if self.optr == cython.NULL:
             return None
-        elif self.iptr == NULL:
+        elif self.iptr == cython.NULL:
             return self
         else:
-            return build_container_format(NULL, self.optr)
+            return build_container_format(cython.NULL, self.optr)
 
     @property
     def is_input(self):
-        return self.iptr != NULL
+        return self.iptr != cython.NULL
 
     @property
     def is_output(self):
-        return self.optr != NULL
+        return self.optr != cython.NULL
 
     @property
     def long_name(self):
@@ -115,7 +128,7 @@ cdef class ContainerFormat:
 
     @property
     def extensions(self):
-        cdef set exts = set()
+        exts: cython.set = set()
         if self.iptr and self.iptr.extensions:
             exts.update(self.iptr.extensions.split(","))
         if self.optr and self.optr.extensions:
@@ -129,9 +142,8 @@ cdef class ContainerFormat:
 
         :rtype: int
         """
-        return (
-            (self.iptr.flags if self.iptr else 0) |
-            (self.optr.flags if self.optr else 0)
+        return (self.iptr.flags if self.iptr else 0) | (
+            self.optr.flags if self.optr else 0
         )
 
     @property
@@ -139,32 +151,34 @@ cdef class ContainerFormat:
         return bool(self.flags & lib.AVFMT_NOFILE)
 
 
-cdef get_output_format_names():
-    names = set()
-    cdef const lib.AVOutputFormat *ptr
-    cdef void *opaque = NULL
+@cython.cfunc
+def get_output_format_names():
+    names: cython.set = set()
+    ptr: cython.pointer[cython.const[lib.AVOutputFormat]]
+    opaque: cython.p_void = cython.NULL
     while True:
-        ptr = lib.av_muxer_iterate(&opaque)
+        ptr = lib.av_muxer_iterate(cython.address(opaque))
         if ptr:
             names.add(ptr.name)
         else:
             break
     return names
 
-cdef get_input_format_names():
-    names = set()
-    cdef const lib.AVInputFormat *ptr
-    cdef void *opaque = NULL
+
+@cython.cfunc
+def get_input_format_names():
+    names: cython.set = set()
+    ptr: cython.pointer[cython.const[lib.AVInputFormat]]
+    opaque: cython.p_void = cython.NULL
     while True:
-        ptr = lib.av_demuxer_iterate(&opaque)
+        ptr = lib.av_demuxer_iterate(cython.address(opaque))
         if ptr:
             names.add(ptr.name)
         else:
             break
     return names
+
 
 formats_available = get_output_format_names()
 formats_available.update(get_input_format_names())
-
-
 format_descriptor = wrap_avclass(lib.avformat_get_class())
